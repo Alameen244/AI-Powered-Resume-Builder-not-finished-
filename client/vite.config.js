c275b94c585f51c0b041a96658eaa430ad852150
alameenpurkait244@gmail.com <alameenpurkait244@gmail.com>
2026-09-04 20:21:54 +0530

    build for hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI-Powered-Resume-Builder-not-finished-/',
   server: {
     host: '127.0.0.1',
     port: 5173,
